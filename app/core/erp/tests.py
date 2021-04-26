cdbb9bd202d6c680c93d216096f79e173d569ef8
Danilo ortiz <danilo_ort@hotmail.com>
2021-04-26 15:35:17 -0500

    carga de archivos del proyecto

---
+++ app/core/erp/tests.py
@@ -0,0 +1,19 @@
+from config.wsgi import *
+from core.erp.models import Type
+# listar
+#query = Type.objects.all()
+#print(query)
+# insertar
+#t = Type()
+#t.name = 'Gerente'
+#t.save()
+
+# ediccion
+#t = Type.objects.get(id=2)
+#t.name = 'Gerente'
+#t.save()
+#print(t)
+#eliminar
+#t = Type.objects.get(id=2)
+#t.delete()
+
